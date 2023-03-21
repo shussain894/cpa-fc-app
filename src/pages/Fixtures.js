86fa8b627d6e92df98539008a6e97c07a2e58234
Shah Hussain <shahhussain1@me.com>
2023-03-21 12:13:07 +0000

    added a checkbox

--- src/pages/Fixtures.js
+++ src/pages/Fixtures.js
@@ -9,7 +9,24 @@ const Fixtures = ({ navigate }) => {
   const handleSubmit = async (event) => {
     event.preventDefault();
     console.log(`${opponent}, ${venue}, ${date}`)
+    setOpponent("")
+    setVenue("")
+    setDate("")
   }
+
+  const Checkbox = ({ label }) => {
+
+    const [isChecked, setIsChecked] = useState(false);
+
+    return (
+      <div>
+        <label>
+          <input type="checkbox" checked={isChecked} />
+          <span>{label}</span>
+        </label>
+      </div>
+    );
+  };
 
   const handleOpponentChange = (event) => {
     setOpponent(event.target.value)
@@ -30,11 +47,21 @@ const Fixtures = ({ navigate }) => {
           <h2 className='title'>  Welcome to CPA FC Fixtures! </h2>
          
           <form onSubmit={handleSubmit}>
-          <input placeholder="Opponent" id="opponent" type='text' value={ opponent } onChange={handleOpponentChange}/>
-          <input placeholder="Venue" id="venue" type='text' value={ venue } onChange={handleVenueChange}/>
-          <input placeholder="Date" id="date" type='date' value={ date } onChange={handleDateChange}/>
-          <input id='submit' type="submit" value="Submit"/> 
+            <h3> Add a new fixture! </h3>
+            <input placeholder="Opponent" id="opponent" type='text' value={ opponent } onChange={handleOpponentChange}/>
+            <input placeholder="Venue" id="venue" type='text' value={ venue } onChange={handleVenueChange}/>
+            <input placeholder="Date" id="date" type='date' value={ date } onChange={handleDateChange}/>
+            <input id='submit' type="submit" value="Submit"/> 
           </form>
+        </div>
+        <div>
+        <span style={{ fontWeight: 'bold' }}>Your next fixture:</span>
+          <p> Opponent: </p>
+          <p> Venue: </p>
+          <p> Date: </p>
+          <div>
+            <Checkbox label='Confirm for ' checked={true} />
+          </div>
         </div>
       </body>  
     </>
